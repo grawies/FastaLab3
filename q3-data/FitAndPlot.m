ee8a6cf8582bd0d6c8ccd8264c83b9ef5941f144
Samuel Ljungdahl Zackrisson <samuel.zackrisson@gmail.com>
2015-05-05 11:57:49 +0200

    answer Q3 completely

--- q3-data/FitAndPlot.m
+++ q3-data/FitAndPlot.m
@@ -7,25 +7,26 @@
 
 format long;
 data = load('utanljus.orig.txt');
-U = data(:,2);
+V = data(:,2);
 I = data(:,3);
-initialz = rand(1,2);
+initials = rand(1,2);
 options = optimset('Display','notify');
-params = fminsearch(@diode,initials,options,U,I);
+params = fminsearch(@diode,initials,options,V,I);
 Isat = params(1);
 V0 = params(2);
 
 Ifit = Isat * (1 - exp(-V/V0));
 
-idealityFactor = V0 * 1.60218e-19 / 1.3806488e-23 / 300;
+idealityFactor = V0 * 1.60218e-19 / 1.3806488e-23 / 293.15;
 
 %% nice it up!
 
-plot(V,I,'r*');
+plot(V,I,'r.');
 hold on;
 plot(V,Ifit,'k');
-xlabel('Averaged current I_a_v [A]','FontSize',12)
-ylabel('Voltage U [V]','FontSize',12)
-%legend('cooling','heating')
+ylabel('Current I [A]','FontSize',12)
+xlabel('Voltage U [V]','FontSize',12)
+
 grid on
-title('Voltage over superconducting material','FontSize',12)
+title('IV-plot of unilluminated diode','FontSize',12)
+legend('experimental data', 'fitted equation')
